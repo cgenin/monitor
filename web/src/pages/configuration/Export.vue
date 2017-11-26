7bd9c24b93444d5bf6a6d17ef096700f4ecfd1f4
cgenin <genin.christophe@gmail.com>
2017-11-26 19:38:46 +0100

    end vue migration

--- web/src/pages/configuration/Export.vue
+++ web/src/pages/configuration/Export.vue
@@ -1,8 +1,34 @@
 <template>
-  <h1>Export..</h1>
+  <div class="export-page">
+    <a class="none" ref="link" href="/api/configuration/db/export.json" target="_blank">export</a>
+    <q-btn @click="doExportJson" icon="file_download" class="fullwidthbtn" color="primary">Exporter sous format JSON
+    </q-btn>
+  </div>
 </template>
 <script>
+  import {QBtn} from 'quasar';
+
   export default {
-    name: 'ConfigurationExport'
+    name: 'ConfigurationExport',
+    components: {QBtn},
+    methods: {
+      doExportJson() {
+        this.$refs.link.click();
+      }
+    }
   }
 </script>
+<style scoped>
+  .export-page {
+    margin-top: 2em;
+  }
+
+  .none {
+    display: none;
+  }
+
+  .fullwidthbtn {
+    width: 100%;
+  }
+</style>
+
